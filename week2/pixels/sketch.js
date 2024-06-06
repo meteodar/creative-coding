ba560f536a788bc64f02dc759abe5f55c6902475
meteodar <undergroundsonic1@gmail.com>
2024-06-06 00:54:40 -0400

    update to Pixels

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,10 +1,11 @@
+let img;
 function preload(){
-	img = loadImage();
+  img = loadImage('https://meteodar.github.io/creative-coding/week2/pixels/Spiderman.jpg');
 }
 
 function setup() {
   createCanvas(800, 800);
-  
+  img.loadPixels()
 }
 
 function draw() {
