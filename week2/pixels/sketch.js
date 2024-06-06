1e9146e2b3128ff401c03e24c7974e4a4a3326a3
meteodar <undergroundsonic1@gmail.com>
2024-06-06 02:16:51 -0400

    update

--- week2/pixels/sketch.js
+++ week2/pixels/sketch.js
@@ -1,6 +1,9 @@
 let img;
+let sampleX = 50;
+let sampleY = 50; 
+
 function preload(){
-  img = loadImage('https://meteodar.github.io/creative-coding/week2/pixels/Spiderman.jpg');
+  img = loadImage('https://meteodar.github.io/creative-coding/week2/pixels/spiderman.jpg');
 }
 
 function setup() {
@@ -11,4 +14,14 @@ function setup() {
 function draw() {
   background(220);
 image(img,10,10,100,100);
+
+for (let x = 0; x <= img.width; x += sampleX){
+  for (let y = o; y <= img.height; y += sampleY){
+    var index = (x + y * img.width) * 4;
+    var r = img.pixles[index + 0];
+    var g = img.pixels[index + 1];
+    var b = img.pixels[index + 2];
+
+  }
+}
 }
