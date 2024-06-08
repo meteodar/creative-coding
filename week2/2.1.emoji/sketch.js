37c1deee439cc4d212eb5cf93fbaf0d28f675a6f
meteodar <undergroundsonic1@gmail.com>
2024-06-08 16:22:37 -0400

    emoji complete

--- week2/2.1.emoji/sketch.js
+++ week2/2.1.emoji/sketch.js
@@ -6,6 +6,7 @@ function draw() {
   background(220);
 
   rectMode(CENTER);
+  ellipseMode(CORNER);
 
   //The Letter
   fill("#eee");
@@ -16,28 +17,26 @@ function draw() {
   //Top Left Thick Line 
   stroke("#000");
   strokeWeight(5);
-  line(-147,-97,0,0);
+  line(-148,-97,0,0);
 
   //Top Right Thick Line
-  line(147,-97,0,0);
+  line(148,-97,0,0);
 
   //Bottom Left Thin Line
   stroke("#888");
   strokeWeight(2);
-  line(0,0,-147,97);
+  line(0,0,-148,97);
 
   //Bottom Right Thin Line
-  line(0,0,147,97)
+  line(0,0,148,97)
 
   //Bottom of Heart(triangle)
-  stroke("#821002");
   fill("#d61a03");
   triangle(-74,-50,0,55,74,-50);
 
-  //Left-Side of Heart
-  //ellipse (-37,-25,-74,20);
-  //rotateY(15);
-
-  //Right Side of Heart 
+  //Heart Side 1
+  ellipse (-70,-74,74,50);
   
+  //Heart Side 2 
+  ellipse (0,-74,70,50);
 }
