318756006d56073722637463601a0a636d836cc4
meteodar <undergroundsonic1@gmail.com>
2024-06-14 22:14:40 -0400

    follower

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -1,7 +1,45 @@
+let speedfactor = 3;
+let xspeed = speedfactor;
+let yspeed = speedfactor;
+let x = 0;
+let y = 0;
+let d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+
+//https://www.hiclipart.com/free-transparent-background-png-clipart-hpvxw#google_vignette
+function preload() {
+  image = loadImage()
+}
+
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(800, 800);
 }
 
 function draw() {
   background(220);
+
+preload
+   if (mouseX > x){
+   xspeed = speedfactor;
+ }else{
+   xspeed = -speedfactor;
+ }
+
+ if (mouseY > y){
+   yspeed = speedfactor;
+ }else{
+   yspeed = -speedfactor;
+ }
+
+text("width: " + width, 30, 30);
+text("height: " + height, 30, 45);
+text("mouseX: " + mouseX, 30, 60);
+text("mouseY: " + mouseY, 30, 75);
+
+  //The code must use the distance formula so that 
+  //once the object collides with the cursor 
+  //or the player's graphic, the follower resets to 
+  //a new position on the canvas.
+// distance formula
+
+
 }
