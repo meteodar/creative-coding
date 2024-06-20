4821b0b9ad04a1fade7a6fc76bf98b956ec7039e
meteodar <undergroundsonic1@gmail.com>
2024-06-20 08:56:45 -0400

    size adjustments_follower

--- week3/3.2.follower/sketch.js
+++ week3/3.2.follower/sketch.js
@@ -3,23 +3,47 @@ let xspeed = speedfactor;
 let yspeed = speedfactor;
 let x = 0;
 let y = 0;
-let d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+let d = 0;
+let score = 0;
+let debug = true;
 
 //Background - https://www.cgtrader.com/gallery/project/ratatouille-kitchen
 //Remy - https://www.hiclipart.com/free-transparent-background-png-clipart-hpvxw#google_vignette
+
 function preload() {
-  image = loadImage('https://meteodar.github.io/creative-coding/week3/3.2.follower/ratatouille_remy.jpg');
+  //image source - https://www.hiclipart.com/search?clipart=ratatouille
+  remy = loadImage('https://meteodar.github.io/creative-coding/week3/3.2.follower/Remy.png');
+  //image source - https://www.cgtrader.com/gallery/project/ratatouille-kitchen
+  kitchen = loadImage('https://meteodar.github.io/creative-coding/week3/3.2.follower/kitchen.png');
+  //image source - https://stock.adobe.com/search?k=tomato+cartoon
+  tomato = loadImage('https://meteodar.github.io/creative-coding/week3/3.2.follower/tomato.png');
 }
 
 function setup() {
-  createCanvas(800, 800);
+  createCanvas(1400, 986);
+
+  x = random(width);
+  y = random(height);
+
 }
 
 function draw() {
-  background(220);
+background(kitchen);
+cursor('https://meteodar.github.io/creative-coding/week3/3.2.follower/tomato.png');
 
-preload
-   if (mouseX > x){
+//distance formula
+let d = sqrt((x - mouseX)**2 + (y - mouseY)**2);
+
+//movement
+x += xspeed;
+y += yspeed;
+
+//tomato
+image(remy, x - 49, y - 108);
+
+
+
+if (mouseX > x){
    xspeed = speedfactor;
  }else{
    xspeed = -speedfactor;
@@ -31,10 +55,24 @@ preload
    yspeed = -speedfactor;
  }
 
-text("width: " + width, 30, 30);
-text("height: " + height, 30, 45);
-text("mouseX: " + mouseX, 30, 60);
-text("mouseY: " + mouseY, 30, 75);
+ if (d < 80 ){
+  score += 1;
+  x = random(width);
+  y = random(height);
+
+ }
+
+if (debug){
+textSize(30);
+fill("#ffedcc");
+text("x: " + x, 30, 50);
+text("y: " + y, 30, 80);
+text("mouseX: " + mouseX, 30, 120);
+text("mouseY: " + mouseY, 30, 150);
+text("d:" + d, 30, 180);
+text("score:" + score, 1250, 50);
+ 
+}
 
   //The code must use the distance formula so that 
   //once the object collides with the cursor 
