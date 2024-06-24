8f619680e930ccd66c9c47eed92ec095f73e3051
meteodar <undergroundsonic1@gmail.com>
2024-06-24 02:55:46 -0400

    time

--- week3/time/sketch.js
+++ week3/time/sketch.js
@@ -6,27 +6,61 @@ let phrases = ["Hi, welcome in!", "What brings you in?",
                "The pants are this way!", 
                "The fitting room is over there!", "Have a good day!"];
 let index = 0;
+let debug = false;
 
+function preload(){
 
-//https://www.timeout.com/los-angeles/shopping/
+ // imsge source --> https://www.timeout.com/los-angeles/shopping/
 //best-shops-in-la-best-womens-clothing-boutiques
+store = loadImage('https://meteodar.github.io/creative-coding/week3/time/store.jpg')
+ 
+}
+
 
 
 function setup() {
   createCanvas(800, 800);
+
+  //added frameRate to slow down seconds
   frameRate(0.5);
   textSize(25);
-
+  
 }
 
 function draw() {
-  background(220);
+  background(store);
 
+//defining parameters for minutes operation
 let sales = 500;
 let returns = -200;
 let s = nfp(sales, 4, 2);
 let r = nfp(returns, 4, 2);
 let profit = s*minute() + r*minute();
+
+
+  push();
+
+  //each hour the profit is calculated
+  x_hour = map(hour(),0, 59, 50, 750);
+  text("profit: " + profit, 100, 760);
+  
+  //each minute a hypotheetical amount for sales and items returned are calculated
+  x_minute = map(minute(), 0, 59, 50, 750);
+  text("sales: " + s * minute(), 100, 700)
+  text("returns: " + r * minute(), 100, 730);
+  noStroke();
+  fill("#7a563440");
+  rect(80, 650, 200, 150, 80);
+
+  //each second a phrase is "Spoken" to a customer
+  x_second = map(second(), 0, 59, 50, 650);
+  y_second = map(second(), 0, 59, 50, 650);
+  fill("#eee")
+  text(phrases[index], x_second, y_second);
+  index = int(random(phrases.length));
+   
+   
+  pop();
 
   if (debug){
   text("hour: " + hour(), 50, 50);
@@ -34,23 +68,6 @@ let profit = s*minute() + r*minute();
   text("second:  " + second(), 50, 100);
   }
 
-  push();
-
-  x_hour = map(hour(),0, 59, 50, 750);
-  text("profit: " + profit, 600, 100);
-
-  x_minute = map(minute(), 0, 59, 50, 750);
-  text("sales: " + s * minute(), 600, 50)
-  text("returns: " + r * minute(), 600, 75);
-
-  
-  x_second = map(second(), 0, 59, 50, 650);
-  y_second = map(second(), 0, 59, 50, 650);
-  text(phrases[index], x_second, y_second);
-  index = int(random(phrases.length));
-
-   
-  pop();
 
 
 }
